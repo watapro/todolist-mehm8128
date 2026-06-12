b5bc8c32388d5228200fc4c8fff4d9a3b34a6d18
watapro <watapro2026B1@outlook.jp>
2026-06-12 18:06:54 +0900

    Initial commit

---
+++ eslint.config.ts
@@ -0,0 +1,35 @@
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import { globalIgnores } from 'eslint/config'
+import skipFormatting from 'eslint-config-prettier/flat'
+import pluginOxlint from 'eslint-plugin-oxlint'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import pluginVue from 'eslint-plugin-vue'
+
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{vue,ts,mts,tsx}'],
+  },
+
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+  ...pluginVue.configs['flat/essential'],
+  vueTsConfigs.recommended,
+
+  ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
+
+  skipFormatting,
+
+  {
+    name: 'app/custom-rules',
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+    },
+  },
+)
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import WelcomeMessage from './components/WelcomeMessage.vue'
+</script>
+
+<template>
+  <main>
+    <WelcomeMessage msg="Webエンジニアになろう講習会へようこそ" />
+  </main>
+</template>
+
+<style scoped>
+main {
+  padding: 2rem;
+}
+</style>
